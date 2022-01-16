f15b60fe38f4a64b13d5647c0e566a4fb1d656fc
MeftunH <meftun.hesimli@epigra.com>
2022-01-16 03:30:07 +0300

    First Init

---
+++ src/main/java/problemdomain/IStorage.java
@@ -0,0 +1,8 @@
+package problemdomain;
+
+import java.io.IOException;
+
+public interface IStorage {
+    void updateGameState(SudokuGame game) throws IOException;
+    SudokuGame getGameData() throws IOException;
+}
---
+++ src/main/java/problemdomain/SudokuGame.java
@@ -0,0 +1,23 @@
+package problemdomain;
+
+import java.io.Serializable;
+
+public class SudokuGame implements Serializable {
+    private final GameState gameState;
+    private final int[][] gridState;
+
+    public static final int GRID_BOUNDARY = 9;
+
+    public SudokuGame(GameState gameState, int[][] gridState) {
+        this.gameState = gameState;
+        this.gridState = gridState;
+    }
+
+    public int[][] getCopyOfGridState() {
+        return SudokuUtitites.copyOfGridState(gridState);
+    }
+
+    public GameState getGameState() {
+        return gameState;
+    }
+}
