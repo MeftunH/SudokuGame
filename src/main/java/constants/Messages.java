981078ada8b99176c7e355f1fb808ca5c07a7677
MeftunH <meftun.hesimli@epigra.com>
2022-01-16 03:36:18 +0300

    Add constants

---
+++ src/main/java/constants/Messages.java
@@ -0,0 +1,7 @@
+package constants;
+
+public class Messages {
+    public static final String GAME_COMPLETE = "CONGRATULATIONS! YOU WON!";
+    public static final  String ERROR = "An error has occurred. Please try again.";
+
+}
